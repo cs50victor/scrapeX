90d60bd72123f5b6fe7017043a71ed6b272cafb1
Victor A <52110451+cs50victor@users.noreply.github.com>
2021-06-06 07:03:14 -0400

    laying out structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 
+# Core API development
+## youtube thumbnails, 
+## shopping, stocks and many more other uses
+## go api, python tasks with threading, R statistics, C++
---
+++ whenToUse.go
@@ -0,0 +1,15 @@
+/*
+ - for networking, 
+ - distributed functions 
+ 	(i.e AWS Lambda, firebase functions, notifications), 
+ - cloud services(API's, web server) 
+ - microservices
+ - XML, JSON, CSV parsers
+ - machine learning 
+
+ TOP USE CASES
+ - web development, 
+ - databases 
+ - networking/systems programming
+ - DevOps,
+*/
