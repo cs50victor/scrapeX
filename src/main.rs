c9fc56295d81ffcbb79ab779c849f4b8f9b8991a
Victor A <52110451+cs50victor@users.noreply.github.com>
2023-01-18 22:28:32 -0500

    refactor: simpler

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod utils;
-use actix_web::{web, middleware, App, HttpResponse, HttpServer, Responder};
-use utils::{Response, ResponseType};
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
 use std::error::Error;
+use utils::Response;
 
 const PORT: u16 = 3000;
 
@@ -9,15 +9,13 @@ async fn index() -> impl Responder {
     let url = "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating";
 
     match scrape(url).await {
-        Ok(res) => HttpResponse::Ok().json(Response::new(ResponseType::Data(res))),
-        Err(err) => HttpResponse::NoContent().json(Response::new(ResponseType::Error(err.to_string())))   
+        Ok(res) => HttpResponse::Ok().json(Response::data(res)),
+        Err(err) => HttpResponse::NoContent().json(Response::error(err.to_string())),
     }
-    // HttpResponse::Ok().json(Response::new(ResponseType::Data("rest")))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
     println!("\n[ starting server at http://localhost:{PORT} ]");
     println!("==================================");
 
@@ -26,20 +24,22 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/").to(index))
     })
-    .bind(("127.0.0.1", PORT))?.run().await
+    .bind(("127.0.0.1", PORT))?
+    .run()
+    .await
 }
 
 fn get_selector(_html: &str) -> String {
     "h3.lister-item-header>a".to_string()
 }
 
-async fn scrape(url: &str) -> Result<Vec<String>, Box<dyn Error>>{
+async fn scrape(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let html = reqwest::get(url).await?.text().await?;
     let document = scraper::Html::parse_document(&html);
     let selector = get_selector(&html);
     let title_selector = scraper::Selector::parse(&selector).unwrap();
     let titles = document.select(&title_selector).map(|x| x.inner_html());
-    
+
     let data = titles.map(|item| item).collect::<Vec<String>>();
     Ok(data)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,22 @@
-use serde::{Serialize, Deserialize};
-
-#[allow(dead_code)]
-pub enum ResponseType <T> {
-    Data(T),
-    Error(T),
-}
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Response <T> {
+pub struct Response<T> {
     data: Option<T>,
     error: Option<T>,
 }
 
-impl <T> Response <T> {
-    pub fn new(res:ResponseType<T>) -> Self {
-        match res {
-            ResponseType::Data(data) => Self { data: Some(data), error: None },
-            ResponseType::Error(error) => Self { data: None, error: Some(error) },
+impl<T> Response<T> {
+    pub fn data(data: T) -> Self {
+        Self {
+            data: Some(data),
+            error: None,
+        }
+    }
+    pub fn error(error: T) -> Self {
+        Self {
+            data: None,
+            error: Some(error),
         }
     }
 }
